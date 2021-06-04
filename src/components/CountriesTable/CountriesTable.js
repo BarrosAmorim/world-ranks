f959a335615f81f030daba4f6ab2951448efb13a
BarrosAmorim <barrosamorimd@gmail.com>
2021-06-04 17:28:14 -0300

    components country

--- src/components/CountriesTable/CountriesTable.js
+++ src/components/CountriesTable/CountriesTable.js
@@ -1,8 +1,9 @@
+import Link from 'next/link'
 import styles from './CountriesTable.module.css'
 import { KeyboardArrowDownRounded, KeyboardArrowUpRounded } from '@material-ui/icons'
 import { useState } from 'react'
 
-const orderBy = (countries, value,  direction) => {
+const orderBy = (countries, value, direction) => {
     if (direction === 'asc') {
         return [...countries].sort((a, b) => (a[value] > b[value] ? 1 : -1))
     }
@@ -45,7 +46,7 @@ const CountriesTable = ({ countries }) => {
         } else if (direction === 'desc') {
             setDirection('asc')
         } else {
-            setDirection(null) 
+            setDirection(null)
         }
     }
 
@@ -57,21 +58,23 @@ const CountriesTable = ({ countries }) => {
     return (
         <div>
             <div className={styles.heading}>
-                <button className={styles.heading_name} onClick={()=> setValueDirection('name')}>
+                <button className={styles.heading_name} onClick={() => setValueDirection('name')}>
                     <div>Nome</div>
                     <SortArraw />
                 </button>
-                <button className={styles.heading_population} onClick={()=> setValueDirection('population')}>
+                <button className={styles.heading_population} onClick={() => setValueDirection('population')}>
                     <div>População</div>
                     <SortArraw direction={direction} />
                 </button>
             </div>
-            {orderedCountries.map((country) => {
+            {orderedCountries.map((country, index) => {
                 return (
-                    <div className={styles.row}>
-                        <div className={styles.name}> {country.name} </div>
-                        <div className={styles.population}> {country.population} </div>
-                    </div>
+                    <Link key={index} href={`/country/${country.alpha3Code}`}>
+                        <div className={styles.row}>
+                            <div className={styles.name}> {country.name} </div>
+                            <div className={styles.population}> {country.population} </div>
+                        </div>
+                    </Link>
                 )
             })}
         </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,30 @@
+import { useState } from 'react'
 import CountriesTable from '../components/CountriesTable/CountriesTable'
 import Layout from '../components/Layout/Layout'
 import SearchInput from '../components/SearchInput/SearchInput'
 import styles from './styles/Home.module.css'
 
 export default function Home({ countries }) {
+  console.log(countries)
+  const [keyword, setKeyword] = useState("")
+
+  const filteredCountries = countries.filter(
+    (country) => 
+    country.name.toLowerCase().includes(keyword) ||
+    country.region.toLowerCase().includes(keyword) ||
+    country.subregion.toLowerCase().includes(keyword))
+
+  const onInputChange = (e) => {
+    e.preventDefault()
+    setKeyword(e.target.value.toLowerCase())
+  }
   return (
     <Layout>
       <div className={styles.counts}>
         Pesquisar por {countries.length} Países
       </div>
-      <SearchInput placeholder="Pesquise por Nome, Região ou SubRegião" />
-      <CountriesTable countries={countries} />
+      <SearchInput placeholder="Pesquise por Nome, Região ou SubRegião" onChange={onInputChange} />
+      <CountriesTable countries={filteredCountries} />
     </Layout>
   )
 }
